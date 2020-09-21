627a932862e0f3608fa32362638a60234cb06e61
Anuj Patel <anuj@htree.plus>
2020-09-21 14:54:14 +0530

    testing tree

--- hpt-test/first-t.cc
+++ hpt-test/first-t.cc
@@ -2,10 +2,15 @@
 // Created by anuj on 17/09/20.
 //
 
-//#include <../sql/main.cc>
-//#include <../sql/mysqld.cc>
-#include<stdio.h>
+#include "../storage/xtradb/include/fil0fil.h"
 
 int main() {
+  //tablespace creation
+  dberr_t code = fil_create_new_single_table_tablespace(1, "t1", "TEMP", 0, 0, 1, FIL_ENCRYPTION_OFF, 0);
 
+  //check tablespace
+  ulint check = fil_open_single_table_tablespace(false, false, 1, 0, "TEMP/t1", "t1");
+  if(check == 10) {
+    printf("tablespace exists");
+  }
 }
