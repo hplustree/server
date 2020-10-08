4cb436706566f0e8fac3b9f234e62cde61892e2c
aayushi17 <aayushishah1710@gmail.com>
2020-10-08 18:35:24 +0530

    changes in insert-test file

--- unittest/hpt/insert-t.cc
+++ unittest/hpt/insert-t.cc
@@ -3,9 +3,15 @@
 //
 
 #include "init_n_des.h"
+#include "tablespace.h"
 #include <dict0boot.h>
+#include <que0que.h>
+#include <row0types.h>
+#include <random>
+#include <iostream>
 
-void test_create_index(dict_index_t *index)
+void test_create_index(dict_index_t *index, dict_table_t *table,
+                       char *table_name)
 {
 
   // create table test (id int, name int);
@@ -15,66 +21,82 @@ void test_create_index(dict_index_t *index)
   ulint zip_size= 0;
   index_id_t index_id= (index_id_t) 22;
   ulint n_fields= 5;
-  const char *table_name= "test";
   const char *index_name= "GEN_CLUST_INDEX";
-  dict_table_t table;
   mtr_t mtr;
   ulint root_page_no;
 
-  table.name= (char *) table_name;
-  table.space= space;
-  table.flags= 1;
-  table.flags2= 80;
+  table->name= table_name;
+  table->space= space;
+  table->flags= 1;
+  table->flags2= 80;
 
-  dict_hdr_get_new_id(&table.id, NULL, NULL);
+  dict_hdr_get_new_id(&table->id, NULL, NULL);
 
   dberr_t code= fil_create_new_single_table_tablespace(
-      space, table.name, NULL, dict_tf_to_fsp_flags(table.flags), table.flags2,
-      FIL_IBD_FILE_INITIAL_SIZE, FIL_ENCRYPTION_OFF, 1);
+      space, table->name, NULL, dict_tf_to_fsp_flags(table->flags),
+      table->flags2, FIL_IBD_FILE_INITIAL_SIZE, FIL_ENCRYPTION_OFF, 1);
 
   ok(code == dberr_t::DB_SUCCESS, "Table creation");
 
   index= dict_mem_index_create(table_name, index_name, space, type, n_fields);
 
-  index->table= &table;
+  index->table= table;
   mtr_start(&mtr);
 
-  fsp_header_init(table.space, FIL_IBD_FILE_INITIAL_SIZE, &mtr);
+  fsp_header_init(table->space, FIL_IBD_FILE_INITIAL_SIZE, &mtr);
 
-  //    index->id = 22;
-  //    index->page = FIL_NULL;
-  //    index->name = "GEN_CLUST_INDEX";
-  //    index->table_name = "testing/test";
-  //    index->space = 6;
-  //    index->type = 1;
-  //    index->trx_id_offset = 6;
-  //    index->n_user_defined_cols = 0;
-  //    index->n_uniq =  1;
-  //    index->n_def =  5;
-  //    index->n_fields = 5;
-  //    index->n_nullable = 2;
-  //    index->cached = 1;
-  //    index->to_be_dropped = 0;
-  //    index->online_status = 0;
+//  index->id= index_id;
+//  index->page= FIL_NULL;
+//  index->name= "GEN_CLUST_INDEX";
+//  index->table_name= table_name;
+//  index->space= space;
+//  index->type= 1;
+//  index->trx_id_offset= 6;
+//  index->n_user_defined_cols= 0;
+//  index->n_uniq= 1;
+//  index->n_def= 5;
+//  index->n_fields= 5;
+//  index->n_nullable= 2;
+//  index->cached= 1;
+//  index->to_be_dropped= 0;
+//  index->online_status= 0;
+
   root_page_no= btr_create(type, space, zip_size, index_id, index, &mtr);
   index->page= root_page_no;
   printf("\nroot page number : %lul \n", root_page_no);
 
   page_t *root= btr_root_get(index, &mtr);
+  UT_LIST_ADD_LAST(indexes, table->indexes, index);
+
   ok(mach_read_from_4(root + FIL_PAGE_OFFSET) == root_page_no,
      "Created index");
 }
 
-void test_insert(dict_index_t *index, ulint length)
+void test_insert(dict_index_t *index, dict_table_t *table, ulint length)
 {
   btr_cur_t cursor;
   ulint *offsets= NULL;
   dtuple_t *entry= NULL;
   rec_t *rec;
+  ins_node_t *node=NULL;
   que_thr_t *que_thr= NULL;
   mtr_t mtr;
   big_rec_t *big_rec= NULL;
   mem_heap_t *heap= NULL;
+  byte* mysql_rec=NULL;
+  ulint mysql_row_len= 9;
+  ulint value1, value2;
+
+  // generate random numbers
+  ulint seed = 42;
+  std::vector<ulint> entries(length);
+
+  for (ulint i = 1; i <= length; i++) {
+    entries[i] = i;
+  }
+
+  std::shuffle(entries.begin(), entries.end(),
+               std::default_random_engine(seed));
 
   mtr_start(&mtr);
 
@@ -82,8 +104,32 @@ void test_insert(dict_index_t *index, ulint length)
 
   for (ulint i= 0; i < length; i++)
   {
+    // convert values in bytes; this part will be changed
+    value1 = entries[i];
+    value2 = value1 * 10;
+    mysql_rec = (unsigned char*)value1;
+    strcat((char*)mysql_rec, (char*)value2);
+
+    row_prebuilt_t *pre_built= row_create_prebuilt(table, mysql_row_len);
+
+    row_get_prebuilt_insert_row(pre_built);
+    node = pre_built->ins_node;
+
+    row_mysql_convert_row_to_innobase(pre_built->ins_node->row, pre_built, mysql_rec);
+
+    que_thr= que_fork_get_first_thr(pre_built->ins_graph);
+
+    que_thr->run_node = node;
+    que_thr->prev_node = node;
+
+    ut_ad(dtuple_check_typed(pre_built->ins_node->row));
+
+    row_ins_index_entry_set_vals(node->index, node->entry, node->row);
+
+    ut_ad(dtuple_check_typed(node->entry));
+
     dberr_t err= btr_cur_search_to_nth_level(index, 0, entry, PAGE_CUR_LE,
-                                             BTR_MODIFY_LEAF, &cursor, 2,
+                                             BTR_MODIFY_LEAF, &cursor, 0,
                                              __FILE__, __LINE__, &mtr);
     if (err != DB_SUCCESS)
     {
@@ -121,18 +167,22 @@ int main(int argc __attribute__((unused)), char *argv[])
 
   // setup
   setup();
-  //  init_available_charsets();
 
   // test1: create tablespace
+  const char *table_name= "test";
   dict_index_t index;
-  test_create_index(&index);
+  dict_table_t table;
+  test_create_index(&index, &table, (char *) table_name);
 
   // test: insert operation
-  ulint length= 1000;
-  test_insert(&index, length);
+  ulint length= 10;
+  test_insert(&index, &table, length);
 
-  //  destroy();
-  remove_local_files();
+  destroy();
+
+  // delete the created tablespace file
+  // always run it after destruction
+  delete_tablespace_ibd_file((char *) table_name);
 
   my_end(0);
   return exit_status();
